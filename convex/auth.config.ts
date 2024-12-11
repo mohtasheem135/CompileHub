de898102725597ce03c8591c34b1b2811c497f47
mohtasheem135 <mohtasheemejaz@gmail.com>
2024-12-11 21:56:23 +0530

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://positive-terrapin-0.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+function page() {
+  console.log("This is the about page....");
+  return <div>About page...</div>;
+}
+
+export default page;
